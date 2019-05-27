2a52712cddd0b579692045de7aae99225f806da7
nankiewiczka <karolina.nankiewicz@gmail.com>
2019-05-27 23:28:21 +0200

    Create react project

---
+++ web/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>KanbanBoard</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
