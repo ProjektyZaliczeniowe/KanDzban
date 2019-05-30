177aabf393ba7046feae6288810e08bacbc6d818
Krzysztof <krzysiek251p@gmail.com>
2019-05-30 17:07:57 +0200

    added CrossOrigin

--- server/src/main/java/kanban/board/server/resource/LabelResource.java
+++ server/src/main/java/kanban/board/server/resource/LabelResource.java
@@ -15,24 +15,28 @@ public class LabelResource {
         this.labelService = labelService;
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @GetMapping("/labels")
     public ResponseEntity getAllLabels() {
         return new ResponseEntity<>(labelService.getAllLabels(), HttpStatus.OK);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @PostMapping("/labels")
     public ResponseEntity addLabel(@RequestBody Label label) {
         return new ResponseEntity<>(labelService.add(label), HttpStatus.CREATED);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @DeleteMapping("/labels/{id}")
     public ResponseEntity deleteLabel(@PathVariable String id) {
         labelService.deleteLabelById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
-    @PutMapping("/tasks")
-    public ResponseEntity editLabel(@RequestBody Label task) {
-        return new ResponseEntity<>(labelService.modify(task), HttpStatus.OK);
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
+    @PutMapping("/labels")
+    public ResponseEntity editLabel(@RequestBody Label label) {
+        return new ResponseEntity<>(labelService.modify(label), HttpStatus.OK);
     }
 }
--- server/src/main/java/kanban/board/server/resource/TaskResource.java
+++ server/src/main/java/kanban/board/server/resource/TaskResource.java
@@ -15,27 +15,32 @@ public class TaskResource {
         this.taskService = taskService;
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @GetMapping("/tasks")
     public ResponseEntity getTasks() {
         return new ResponseEntity<>(taskService.getAll(), HttpStatus.OK);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @PostMapping("/tasks")
     public ResponseEntity addTask(@RequestBody Task task) {
         return new ResponseEntity<>(taskService.add(task), HttpStatus.CREATED);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @DeleteMapping("/tasks/{id}")
     public ResponseEntity deleteTask(@PathVariable String id) {
         taskService.deleteTaskById(id);
         return new ResponseEntity(HttpStatus.NO_CONTENT);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @GetMapping("/tasks/{id}/users")
     public ResponseEntity getAllTaskUsers(@PathVariable String id) {
         return new ResponseEntity<>(taskService.getTaskUsers(id), HttpStatus.OK);
     }
 
+    @CrossOrigin(allowedHeaders = "*", allowCredentials = "true")
     @PutMapping("/tasks")
     public ResponseEntity editTask(@RequestBody Task task) {
         return new ResponseEntity<>(taskService.modify(task), HttpStatus.OK);
