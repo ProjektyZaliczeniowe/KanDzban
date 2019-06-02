56ac87f761faa4b7d7047be76455f21034eb1072
nankiewiczka <karolina.nankiewicz@gmail.com>
2019-06-02 20:23:54 +0200

    changes in navbar

--- web/src/App.js
+++ web/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Navbar, Nav } from "react-bootstrap";
 import LoginPage from "./components/LoginPage";
 import RegisterPage from "./components/RegisterPage";
 
@@ -8,17 +9,13 @@ function App() {
   return (
     <Router>
       <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/login">Sign in</Link>
-          </li>
-          <li>
-            <Link to="/register">Sign up</Link>
-          </li>
-        </ul>
+        <Navbar bg="dark" variant="dark">
+          <Navbar.Brand href="/">KANBANBoard</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link href="login">Sign in</Nav.Link>
+            <Nav.Link href="register">Sign up</Nav.Link>
+          </Nav>
+        </Navbar>
 
         <hr />
 
