74f8a734407c9c2c56bfa21d61a9988f3eca6457
Krzysztof <krzysiek251p@gmail.com>
2019-06-23 16:12:21 +0200

    task action endpoint selecting actions with specific user

--- server/src/main/java/kanban/board/server/repository/ActionLogRepository.java
+++ server/src/main/java/kanban/board/server/repository/ActionLogRepository.java
@@ -2,6 +2,10 @@ package kanban.board.server.repository;
 
 import kanban.board.server.model.ActionLog;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 public interface ActionLogRepository extends MongoRepository<ActionLog, String> {
+    List<ActionLog> getActionLogByObjectType(String objectType);
 }
--- server/src/main/java/kanban/board/server/resource/ActionLogResource.java
+++ server/src/main/java/kanban/board/server/resource/ActionLogResource.java
@@ -1,7 +1,27 @@
 package kanban.board.server.resource;
 
+import kanban.board.server.model.ActionLog;
+import kanban.board.server.model.Task;
+import kanban.board.server.repository.ActionLogRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ActionLogResource {
+    private final ActionLogRepository actionLogRepository;
+
+    public ActionLogResource(ActionLogRepository actionLogRepository) {
+        this.actionLogRepository = actionLogRepository;
+    }
+
+    @GetMapping("/logActions/tasks/users/{userId}")
+    public ResponseEntity getTaskActionsByUserId(@PathVariable String userId) {
+        List<ActionLog> actionLogs = actionLogRepository.getActionLogByObjectType(Task.class.getName());
+        return new ResponseEntity<>(actionLogs.stream().filter(l -> l.getParams().get("assignedUserIds").contains(userId)), HttpStatus.OK);
+    }
 }
